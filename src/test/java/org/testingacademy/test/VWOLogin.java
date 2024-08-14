6801fbca4e9eef85fd096fd8a582aba5d867ea75
Rakesh R <rakeshrake5398@gmail,com>
2024-08-14 18:42:40 +0530

    Basic Test Case

--- src/test/java/org/testingacademy/test/VWOLogin.java
+++ src/test/java/org/testingacademy/test/VWOLogin.java
@@ -2,6 +2,6 @@ package org.testingacademy.test;
 
 public class VWOLogin {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hellooo");
     }
 }
